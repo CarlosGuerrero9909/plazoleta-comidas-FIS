13dfbd95a37f19b4afb6477049e74a0eb5a19127
SEBASTIAN-MORENO-ROJAS-UD <semorenor@udistrital.edu.co>
2023-06-01 16:56:09 -0500

    Se completa el pago y actualizacion en la BD de los stocks y creacion del pedido

--- controllers/transaccion.js
+++ controllers/transaccion.js
@@ -1,29 +1,57 @@
 const transaccionRouter = require('express').Router()
 const Menu = require('../models/menu')
+const Pedido = require('../models/pedido')
 const Producto = require('../models/producto')
 const calcularIva = require('./menus').calcularIva
 const decodificarToken = require('../utils/loginSecurity')
 require('../utils/carritoGlobal')
 
 transaccionRouter.get('/disponibilidad', async (request, response) => {
-  const usuario = await decodificarToken(request)
-  if (!usuario) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-
   const body = request.body
   const menus = body?.menus
 
+  global.globalCarrito = []
+
+  const productos = []
+
   for (const menu of menus) {
-    const menuBD = await Menu.findById(menu.id)
-    const productos = menuBD.productos
-    for (const producto of productos) {
-      const productoBD = await Producto.findById(producto)
-      if (productoBD.stockProductoSimple < menu.quantity) {
-        global.globalCarrito = []
-        return response.json({ resultado: false })
+    const idProductos = menu.productos
+
+    for (const idProducto of idProductos) {
+      const productoBD = await Producto.findById(idProducto)
+
+      let repetido = false
+
+      for (let i = 0; i < productos.length; i++) {
+        if (productos[i].nombre === productoBD.nombre) {
+          productos[i].cantidad += menu.quantity
+          repetido = true
+          break
+        }
+      }
+
+      if (!repetido) {
+        const producto = {
+          id: productoBD.id,
+          nombre: productoBD.nombre,
+          cantidad: menu.quantity,
+          stockProductoSimple: productoBD.stockProductoSimple
+        }
+        productos.push(producto)
       }
     }
+  }
+
+  for (const producto of productos) {
+    if (producto.stockProductoSimple < producto.cantidad) {
+      global.globalCarrito = []
+      return response.json({ resultado: false })
+    }
+  }
+
+  console.log(productos)
+
+  for (const menu of menus) {
     global.globalCarrito.push(menu)
   }
 
@@ -49,6 +77,7 @@ transaccionRouter.post('/pago', async (request, response) => {
   const dinero = Math.floor(Math.random() * (100000 - 40000) + 40000)
 
   if (dinero >= totalPago) {
+    const menus = []
     for (const menu of global.globalCarrito) {
       const menuBD = await Menu.findById(menu.id)
       const productos = menuBD.productos
@@ -61,13 +90,24 @@ transaccionRouter.post('/pago', async (request, response) => {
           await Producto.findByIdAndUpdate(productoBD.id, newStockSimple, { new: true })
         }
       }
+      menus.push(menu.id)
     }
+
+    const pedido = new Pedido({
+      fecha: new Date(),
+      estado: 'Preparando',
+      precioTotal: totalPago,
+      menu: menus,
+      cliente: usuario.id
+    })
+
+    await pedido.save()
 
     global.globalCarrito = []
 
-    return response.json({ resultado: true })
+    return response.json({ resultado: 'pago exitoso' })
   } else {
-    return response.status(400).json({ resultado: false })
+    return response.status(400).json({ resultado: 'no hay dinero' })
   }
 })
 
